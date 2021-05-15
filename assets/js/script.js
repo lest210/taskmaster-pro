8d2462e8441165951e0daf3a582dc182fbbb7ecf
lest210 <lest210@gmail.com>
2021-05-14 22:58:25 -0500

    updated drag and drop, added mobile responsivness

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,7 +120,55 @@ $(".list-group").on("blur", "input[type='text']", function(){
   $(this).replaceWith(taskSpan);
 })
 
+$(".card .list-group").sortable({
+    connectWith: $(".card .list-group"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    activate: function(event) {
+      console.log("activate",this);
+    },
+    deactivate: function(event) {
+      console.log("deactivate",this);
+    },
+    over: function(event) {
+      console.log("over", event.target);
+    },
+    out: function(event) {
+      console.log("out", event.target);
+    },
+    update: function(event) {
+      var tempArr = [];
+    
 
+      $(this).children().each(function() {
+        var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+        console.log($(this));
+        
+        var arrName = $(this)
+        .attr("class")
+        .replace("list-", "");
+        
+
+        var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+        tempArr.push({
+          text: text,
+          date: date
+        });
+       
+        tasks[arrName] = tempArr;
+        saveTasks();
+      });
+      console.log (tempArr);
+    }
+});
 
 
 
@@ -171,5 +219,22 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
+
 
 
